6077063946ba2275564eef1c71fed5d175c6237e
Jules <ju.pabst@gmail.com>
2023-12-19 16:53:10 +0100

    connected mongoDB

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,15 @@
+require("dotenv").config({ path: "../.env" });
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+
+const mongoDBUri = process.env.mongoDBUri;
+
+mongoose
+  .connect(mongoDBUri)
+  .then(() => console.log("Successfully connected to MongoDB."))
+  .catch((err) => console.error("Connection error", err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
