abf78044dddf66aea7a29541bbcc01ecf638c556
Jules <ju.pabst@gmail.com>
2023-12-21 21:13:32 +0100

    added cors, added grid system to Overview Component

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,8 @@ require("dotenv").config({ path: "../.env" });
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
+app.use(cors());
 
 const mongoose = require("mongoose");
 const mongoDBUri = process.env.mongoDBUri;
--- client/src/Overview.js
+++ client/src/Overview.js
@@ -1,6 +1,34 @@
 import React from "react";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Overview() {
-  return <div>overview</div>;
+  const [wines, setWines] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/wines")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setWines(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <Container>
+      <Row className="justify-content-md-center">
+        {wines.map((wine) => (
+          <Col xs="12" lg="4">
+            <p>
+              Hi
+              {wine.name} - {wine.taste}
+            </p>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
